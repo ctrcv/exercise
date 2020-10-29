7322ce1f7fef32668f96fe9bc45b8d242b8fe6c7
ctrcv <1723971842@qq.com>
2020-10-29 16:20:34 +0800

    简洁—官方题解版

--- myScripts/129. 求根到叶子节点数字之和.py
+++ myScripts/129. 求根到叶子节点数字之和.py
@@ -14,23 +14,34 @@ class Solution(object):
         if not root:
             return 0
 
-        paths = []
+        # paths = []
+        #
+        # def dfs(node, path):
+        #     path.append(str(node.val))
+        #     if not node.left and not node.right:
+        #         # print (path)
+        #         paths.append(''.join(path))
+        #         return
+        #
+        #     if node.left:
+        #         dfs(node.left, path)
+        #         path.pop()
+        #     if node.right:
+        #         dfs(node.right, path)
+        #         path.pop()
+        #
+        # dfs(root, [])
+        # tmp = [int(s) for s in paths]
+        # ret = sum(tmp)
+        # return ret
 
-        def dfs(node, path):
-            path.append(str(node.val))
+        def dfs(node, prevTotal):
+            if not node:
+                return 0
+            total = prevTotal * 10 + node.val
             if not node.left and not node.right:
-                # print (path)
-                paths.append(''.join(path))
-                return
+                return total
+            else:
+                dfs(node.left, total) + dfs(node.right, total)
 
-            if node.left:
-                dfs(node.left, path)
-                path.pop()
-            if node.right:
-                dfs(node.right, path)
-                path.pop()
-
-        dfs(root, [])
-        tmp = [int(s) for s in paths]
-        ret = sum(tmp)
-        return ret
+        return dfs(root, 0)
