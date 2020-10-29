6b1f9958fc1bbc3514a7682f834cb6d68650cee7
ctrcv <1723971842@qq.com>
2020-10-29 09:44:58 +0800

    20.10.29二叉树打卡

---
+++ myScripts/129. 求根到叶子节点数字之和.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        paths = []
+
+        def dfs(node, path):
+            path.append(str(node.val))
+            if not node.left and not node.right:
+                # print (path)
+                paths.append(''.join(path))
+                return
+
+            if node.left:
+                dfs(node.left, path)
+                path.pop()
+            if node.right:
+                dfs(node.right, path)
+                path.pop()
+
+        dfs(root, [])
+        tmp = [int(s) for s in paths]
+        ret = sum(tmp)
+        return ret
